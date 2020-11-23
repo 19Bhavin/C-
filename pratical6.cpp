9deabf2475bcb0c28d9f60eaa2f2396661660c2f
19Bhavin <73989718+19Bhavin@users.noreply.github.com>
2020-11-23 11:29:06 +0530

    Add files via upload

---
+++ pratical6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Person{
+	public:
+	string name;
+	int age;
+	};
+
+int main(){
+	Person obj;
+	obj.name="Bhavin Jain";
+	obj.age=18;
+	cout<<obj.name <<"\n";
+	cout<<obj.age <<"\n";
+}
